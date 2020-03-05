ea19d17d1d36f6ce6cc29214c42f51194b8ebda0
=VuHaiDuong99 <=>
2020-03-05 22:00:49 +0700

    'updateproduct'

--- src/main/java/com/sapo/apiclothes/controller/ApiProductDetail.java
+++ src/main/java/com/sapo/apiclothes/controller/ApiProductDetail.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
-@Controller
+
 @RequestMapping(value ="/api/productdetail")
 public class ApiProductDetail {
     @Autowired
